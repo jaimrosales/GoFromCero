6023ab6fbef77257a5af3360d5471b411c383553
jaimrosales <jaimrosales@tesla.com>
2025-06-11 02:13:55 -0500

    se crea el archivo go.mod, que permite correr en terminal el comando <go run . -p -blablabla> ademas se agregan flags y su tipo para indicar los flags a usar en la terminal   <-p,-a,-n,-t,-r -s>, se documento

--- Edls/main.go
+++ Edls/main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+)
+
 func main() {
 
+	//filter flags   	flags de filtrado de salida
+	flagPattern := flag.String("p", "", "filter by pattern")           // nos permite seleccionar el patron de archivo
+	flagAll := flag.Bool("a", false, "all files including hide files") // nos mostrara todos los archivos
+	flagNumberRecords := flag.Int("n", 0, "number of archivos")        // indica el numero maximo de archivos que se desean revisar en el filtrado
+
+	//order flags  		flags que permitiran organizar la salida
+	hasOrderbytime := flag.Bool("t", false, "sort by time, oldest first")
+	hasOrderbySize := flag.Bool("s", false, "sort by file size, smallest first")
+	hasOrderReverse := flag.Bool("r", false, "reverse order while sorting")
+
+	flag.Parse() //un flag es con el que podremos meter comandos -h o --h esta parte nos permite ver mas que la direccion de memoria
+
+	fmt.Println("patron de filtro ", *flagPattern)
+	fmt.Println("all", *flagAll)
+	fmt.Println("flagNumberRecords: ", *flagNumberRecords)
+	fmt.Println("hasOrderbytime: ", *hasOrderbytime)
+	fmt.Println("hasOrderbySize: ", *hasOrderbySize)
+	fmt.Println("hasOrderReverse: ", *hasOrderReverse)
 }
