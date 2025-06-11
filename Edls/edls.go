b7d0fc944e7d51e28fd7552cf4d3638b8b870f51
jaimrosales <jaimrosales@tesla.com>
2025-06-10 23:48:56 -0500

    se anaden estructuras y se crea el mapa pero no se define, video 3_7 minuto 7:10

--- Edls/edls.go
+++ Edls/edls.go
@@ -1,5 +1,7 @@
 package main
 
+import "time"
+
 //files types
 const ( //constantes a utilizar en el edls
 	fileRegular int = iota
@@ -22,3 +24,32 @@ const (
 	jpg = ".jpg"
 	gif = ".gif"
 )
+
+//la siguiente estructura contendra los diferentes campos de un archivo necesarios para renderizar la informacion
+type file struct {
+	name             string
+	fileType         int
+	isDir            bool
+	isHidden         bool
+	userName         string
+	groupname        string
+	size             int64
+	modificationTime time.Time
+	mode             string
+}
+
+//en la siguiente escritura se coontendra el estilos de renderizado
+type styleFileType struct {
+	incon  string
+	color  string
+	symbol string
+}
+
+var mapStylesByFileType = map[int]styleFileType{
+	fileRegular:    {},
+	fileDirectory:  {},
+	fileExecutable: {},
+	fileCompress:   {},
+	fileImage:      {},
+	fileLink:       {},
+}
