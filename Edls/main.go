e281d29e0ebb7f7bec0cdbeffd3dc05c4cb4d448
jaimrosales <jaimrosales@tesla.com>
2025-06-17 03:47:44 -0500

    se completo proyecto parte 1 4.10, por alguna razon marcaba un error en la fila 71 con el return de f, no completamente seguro de si se resolvio, al momento del commit, solo elimine f, y volvi a escribir f

--- Edls/main.go
+++ Edls/main.go
@@ -1,8 +1,16 @@
 package main
 
+//flags: nos permiten indicar alguna accion o filtro dentro del comando como --all --help --decorate son ejemplos, para poder escribirlos desde
+//		desde la pantalla usamos el flag.parse que nos permitira ingresar el parse
+
+//argument: indican la direccion, por lo general se ingresan despues de los flags, son los documents/blebleble/user/ ble/ pictures,
+//			para poder interactuar con ellos en el codigo se utiliza el flag.arg despues del flag.parse
+
 import (
 	"flag"
 	"fmt"
+	"io/fs"
+	"os"
 )
 
 func main() {
@@ -13,16 +21,39 @@ func main() {
 	flagNumberRecords := flag.Int("n", 0, "number of archivos")        // indica el numero maximo de archivos que se desean revisar en el filtrado
 
 	//order flags  		flags que permitiran organizar la salida
-	hasOrderbytime := flag.Bool("t", false, "sort by time, oldest first")
-	hasOrderbySize := flag.Bool("s", false, "sort by file size, smallest first")
-	hasOrderReverse := flag.Bool("r", false, "reverse order while sorting")
+	//hasOrderbytime := flag.Bool("t", false, "sort by time, oldest first")
+	//hasOrderbySize := flag.Bool("s", false, "sort by file size, smallest first")
+	//hasOrderReverse := flag.Bool("r", false, "reverse order while sorting")
 
-	flag.Parse() //un flag es con el que podremos meter comandos -h o --h esta parte nos permite ver mas que la direccion de memoria
+	flag.Parse()        //un flag es con el que podremos meter comandos -h o --h esta parte nos permite ver mas que la direccion de memoria
+	path := flag.Arg(0) // flag.arg nos regresa ela rgumento que se ah ingresa, en este programa se le asigna el string a la variable path, nos regresara el primer argumento la ruta
+	if path == "" {
+		path = "."
+	}
+	dirs, err := os.ReadDir(path) //Se lee el directorio enviado por el usuario, y se asigna a dirs, en caso de que haya un error se asigna a err
+	if err != nil {               //se controla el error por medio de un panico
+		panic(err)
+	}
 
+	fs := []file{} //este slice es donde se guardaran todos los archivos dentro de la direccion que se mostraran al llamarse la funcion edls
+	for _, dir := range dirs {
+		f, err := getFile(dir, false)
+		if err != nil { //se controla el error por medio de un panico
+			panic(err)
+		}
+
+		fs = append(fs, f)
+
+	}
+
+	fmt.Println(fs)
 	fmt.Println("patron de filtro ", *flagPattern)
 	fmt.Println("all", *flagAll)
 	fmt.Println("flagNumberRecords: ", *flagNumberRecords)
-	fmt.Println("hasOrderbytime: ", *hasOrderbytime)
-	fmt.Println("hasOrderbySize: ", *hasOrderbySize)
-	fmt.Println("hasOrderReverse: ", *hasOrderReverse)
+
+}
+func getFile(dir fs.DirEntry, isHidden bool) (file, error) {
+
+	return file{}, nil
+
 }
